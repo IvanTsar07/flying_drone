78809d2bb97938b7bd8758006e26c92be73bae31
Ivan Tsarehorodtsev <i.tsarehorodtsev@kasta.software>
2024-06-10 19:17:22 +0300

    first commit

---
+++ src/config.cpp
@@ -0,0 +1,49 @@
+#include "config.hpp"
+
+class PeriodicTask
+{
+  unsigned long previousMillis;
+  unsigned long interval;
+  void (*task)();
+
+public:
+  PeriodicTask(unsigned long _interval, void (*_task)())
+  {
+    interval = _interval;
+    task = _task;
+    previousMillis = 0;
+  }
+
+  void run()
+  {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval)
+    {
+      previousMillis = currentMillis;
+      (*task)();
+    }
+  }
+};
+
+void turnOFF_display(void)
+{
+  digitalWrite(TFT_BL, 0);
+}
+
+void read_Joystick(bool which, byte *data[2])
+{
+  switch (which)
+  {
+  case LEFT_JOY:
+    /* code */
+    data[0] = analogRead(Joy1_X) >> 2;
+    data[1] = analogRead(Joy1_Y) >> 2;
+    break;
+
+  case RIGHT_JOY:
+    /* code */
+    data[0] = analogRead(Joy2_X) >> 2;
+    data[1] = analogRead(Joy2_Y) >> 2;
+    break;
+  }
+}
---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include "config.hpp"
+
+byte *joyLeftValues[2];
+byte *joyRightValue[2];
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(TFT_BL, OUTPUT);
+  analogWrite(TFT_BL, 200);
+  tft.init(240, 320);
+  tft.fillScreen(ST77XX_BLUE);
+  tft.setRotation(2);
+  tft.setTextSize(1);
+  tft.setCursor(10, 40);
+  tft.print("Hello");
+}
+
+PeriodicTask DETECT_LEFT_JOYSTICK_VALUE(100, joystick1_read), DETECT_RIGHT_JOYSTICK_VALUE(100, joystick2_read);
+
+void loop()
+{
+  DETECT_LEFT_JOYSTICK_VALUE.run();
+  DETECT_RIGHT_JOYSTICK_VALUE.run();
+
+  if (millis() % 100 == 0)
+  {
+    tft.setCursor(10, 10);
+    tft.print("Joystick 1X:   ");
+    tft.print(String(joyLeftValues[0]));
+
+    tft.setCursor(10, 20);
+    tft.print("Joystick 1Y:   ");
+    tft.print(String(joyLeftValues[1]));
+
+    tft.setCursor(20, 10);
+    tft.print("Joystick 2X:   ");
+    tft.print(String(joyRightValue[0]));
+
+    tft.setCursor(20, 20);
+    tft.print("Joystick 2Y:   ");
+    tft.print(String(joyRightValue[1]));
+  }
+}
+
+void joystick1_read()
+{
+  read_Joystick(LEFT_JOY, joyLeftValues);
+}
+
+void joystick2_read()
+{
+  read_Joystick(RIGHT_JOY, joyRightValue);
+}
